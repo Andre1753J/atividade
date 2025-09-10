9f2299f5fb27a3aec148c3ced6f391c32a23fa2a
Andre1753J <andreorocondo@gmail.com>
2025-09-10 14:50:31 -0400

    atividade 1 pronta front

--- App.tsx
+++ App.tsx
@@ -1,44 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, FlatList, StyleSheet, ListRenderItem } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import { Tabela } from './src/components';
+import { styles } from './styles';
+
+type Produto = {
+  id: string;
+  nome: string;
+  status: boolean;
+};
 
 export default function App() {
-  const [item, setItem] = useState<string>();
+  const [produto, setProduto] = useState<string>('');
+  const [lista, setLista] = useState<Produto[]>([
+    { id: '1', nome: 'Maça', status: true },
+    { id: '2', nome: 'Banana', status: true },
+    { id: '3', nome: 'Melancia', status: true },
+  ]);
+
+  const renderItem: ListRenderItem<Produto> = ({ item }) => (
+    <TouchableOpacity style={styles.item}>
+      <AntDesign name="checkcircle" size={24} color="black" />
+      <Text style={styles.itemText}>{item.nome}</Text>
+    </TouchableOpacity>
+  );
 
   return (
     <View style={styles.container}>
-      <Text style={styles.h1}>Lista de Compras</Text>
-      <StatusBar style="auto" />
-      <TextInput style={styles.textInput} placeholder='Digite o produto' placeholderTextColor='#acc' value={item}></TextInput>
-      <TouchableOpacity style={styles.pesquisa}>
-      <AntDesign name="pluscircle" size={24} color="black"/>
-      </TouchableOpacity>
-      <Tabela produto={item}></Tabela>
+      <Text style={styles.titulo}>Lista do Jorge</Text>
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite o Produto"
+          placeholderTextColor="#aac"
+          value={produto}
+          onChangeText={setProduto}
+        />
+        <TouchableOpacity style={styles.Button}>
+          <AntDesign name="pluscircle" size={24} color="white" />
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <FlatList
+          data={lista}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'cyan',
-    alignItems: 'center',
-  },
-  textInput: {
-    backgroundColor: "black",
-    color: "white",
-    width: 300,
-    marginRight: 90
-  },
-  h1: {
-    color: "black",
-    fontSize: 30,
-    fontWeight: "bold",
-    marginTop: 40,
-  },
-  pesquisa: {
-    alignItems: "center"
-  }
-});
---
+++ styles.ts
@@ -0,0 +1,48 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#6495ED',
+      padding: 20,
+      paddingTop: 60,
+    },
+    titulo: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      marginBottom: 20,
+    },
+    input: {
+      flex: 1,
+      backgroundColor: '#000',
+      color: '#fff',
+      paddingHorizontal: 15,
+      borderRadius: 8,
+      height: 45,
+    },
+    Button: {
+      backgroundColor: '#000',
+      marginLeft: 8,
+      borderRadius: 8,
+      padding: 10,
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 45,
+      width: 45,
+    },
+    item: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingVertical: 10,
+      borderBottomWidth: 5,
+    },
+    itemText: {
+      marginLeft: 10,
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+  });
